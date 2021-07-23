9fb3bcf9dd4e9b7edff29815902e4db92ee7002d
jmd <jordan.michael.dickey@gmail.com>
2021-07-22 20:58:05 -0600

    Created the ExpenseFilter component and made it a controlled component that lifts up state to the Expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+
 import "./Expenses.css";
 
-const Expenses = ({expenses}) => {
+const Expenses = ({ expenses }) => {
+  const [expenseYear, setExpenseYear] = useState('2021');
 
-    const expenseItem = (expenses) => {
-        return expenses.map((expense, index) => <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount} key={index}/>)
-    }
+  const expenseItem = (expenses) => {
+    return expenses.map((expense) => (
+      <ExpenseItem
+        title={expense.title}
+        date={expense.date}
+        amount={expense.amount}
+        key={Math.random().toString()}
+      />
+    ));
+  };
 
-    return (
-        <Card className="expenses">
-            {expenseItem(expenses)}
-        </Card>
-    )
-}
+  const expenseFilterYear = (expenseYearData) => {
+    setExpenseYear(expenseYearData);
+    console.log(expenseYearData);
+  };
 
-export default Expenses
+  return (
+    <div>
+      <Card className="expenses">{expenseItem(expenses)}
+        <ExpensesFilter selected={expenseYear} onExpenseFilterYear={expenseFilterYear} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import ExpenseForm from './ExpenseForm'
-import './NewExpense.css'
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
-const NewExpense = () => {
-    return (
-        <div className="new-expense">
-            <ExpenseForm />
-        </div>
-    )
-}
+const NewExpense = ({onAddExpense}) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    onAddExpense(expenseData);
+  };
 
-export default NewExpense
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
