177551300503e73084970781a91ce68c76559624
jmd <jordan.michael.dickey@gmail.com>
2021-07-24 10:48:05 -0600

    Adding functionality to conditionally render the NewExpense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = ({onSaveExpenseData}) => {
+const ExpenseForm = ({ onSaveExpenseData, onCancel }) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -22,24 +22,27 @@ const ExpenseForm = ({onSaveExpenseData}) => {
     event.preventDefault();
 
     const expenseData = {
-        title: enteredTitle, 
-        amount: enteredAmount,
-        date: new Date(enteredDate)
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
     };
 
     onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredDate('');
-    setEnteredAmount('');
-
-  }
+    setEnteredTitle("");
+    setEnteredDate("");
+    setEnteredAmount("");
+  };
 
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -63,6 +66,9 @@ const ExpenseForm = ({onSaveExpenseData}) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = ({onAddExpense}) => {
+const NewExpense = ({ onAddExpense }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     onAddExpense(expenseData);
+    setIsEditing(!isEditing);
   };
+
+  const startedEditingHandler = event => {
+    setIsEditing(!isEditing);
+  };
+
+  let newExpenseContent= <button onClick={startedEditingHandler}>Add New Expense</button>
+
+  if (isEditing) {
+    newExpenseContent = <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}  onCancel={startedEditingHandler}/>;
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {newExpenseContent}
     </div>
   );
 };
